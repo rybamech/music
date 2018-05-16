b17dca550c288af41b878859bac194d77c74da1c
DimaAdmin <ryba-mech@yandex.ru>
2018-05-16 18:58:24 +0300

    04 - complete search.component and template

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -20,10 +20,41 @@ import {SpotifyService} from '../spotify.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  query: string;
+  results: Object;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private spotify: SpotifyService,
+              private router: Router,
+              private route: ActivatedRoute) {
+    this.route
+      .queryParams
+      .subscribe(params => { this.query = params['query'] || ''; });
   }
 
+  ngOnInit(): void {
+    this.search();
+  }
+
+  submit(query: string): void {
+    this.router.navigate(['search'], { queryParams: { query: query } })
+      .then(_ => this.search() );
+  }
+
+  search(): void {
+    console.log('this.query', this.query);
+    if (!this.query) {
+      return;
+    }
+
+    this.spotify
+      .searchTrack(this.query)
+      .subscribe((res: any) => this.renderResults(res));
+  }
+
+  renderResults(res: any): void {
+    this.results = null;
+    if (res && res.tracks && res.tracks.items) {
+      this.results = res.tracks.items;
+    }
+  }
 }
