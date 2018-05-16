46a42f69c99181dc00e2ab0b8bd1247c7c26fd5e
DimaAdmin <ryba-mech@yandex.ru>
2018-05-16 14:06:54 +0300

    01 - add imports module to app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+import {
+  LocationStrategy,
+  HashLocationStrategy,
+  APP_BASE_HREF
+} from '@angular/common';
 
 
 import { AppComponent } from './app.component';
